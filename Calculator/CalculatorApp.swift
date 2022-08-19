d3f8daa1e6de82c273ed6893cbafdcab6aabf29b
Kevin Taylor <k.a.taylor@protonmail.com>
2022-08-19 14:36:40 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Consultant on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
